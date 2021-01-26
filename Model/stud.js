a4c9879212aa364c4f8f1c10b297638455b7bf36
nnakrani121 <nnakrani121@rku.ac.in>
2021-01-26 16:23:37 +0530

    run in online

--- Model/stud.js
+++ Model/stud.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.schema({
+const schema = mongoose.Schema({
     name:String,
     email:String
     
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const route = require('./route');
 
-mongoose.connect("mongodb+srv://nikhil:1234@cluster0.jk2jh.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedToplogy:true}).then(
+// mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://nikhil:1234@cluster0.jk2jh.mongodb.net/student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
 
     ()=>{
         const app = express();
