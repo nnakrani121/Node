fd4d59e9524d467722c27010f1bc6e872edfac7c
nnakrani121 <nnakrani121@rku.ac.in>
2021-01-26 10:01:27 +0530

    cconst change

--- route.js
+++ route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const stud = require('./Model/stud');
 const router = express.router();
 
-router.get('/studs', async (req,res)=>{
+router.get("/studs", async (req,res)=>{
     
-    const stud = await stud.find();
-    res.send(stud);
+    const stud1 = await stud.find();
+    res.send(stud1);
 });
 
 module.exports = router;
